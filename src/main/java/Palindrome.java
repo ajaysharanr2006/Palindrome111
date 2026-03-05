fbcdad9560ab111103ede7ad34a17fbedf7d6224
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-03-05 16:34:07 +0530

    23mis0669

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -1,7 +1,6 @@
 public class Palindrome{
-    public boolean check(String s){
-     String r=new StringBuilder(s).reverse().toString();
-     return s.equals(r);
-    }
-    }
-    
+public boolean check(String s){
+ String r=new StringBuilder(s).reverse().toString();
+ return s.equals(r);
+}
+}
