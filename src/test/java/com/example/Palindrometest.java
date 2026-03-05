4a2985499a09043618f3806f661e220d99c62066
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-03-05 16:52:39 +0530

    23mis0669

--- src/test/java/com/example/Palindrometest.java
+++ src/test/java/com/example/Palindrometest.java
@@ -1,20 +1,18 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-
 import main.java.Palindrome;
 
 public class Palindrometest {
-    @Test
-    public void test(String s){
-        Palindrome o = new Palindrome();
-        assertEquals('madam',o.check(s));
 
+    @Test
+    public void testPalindrome() {
+        Palindrome o = new Palindrome();
+        assertEquals("madam", o.check("madam"));
     }
 
     @Test
-    public void ntest(String s){
+    public void testNonPalindrome() {
         Palindrome o = new Palindrome();
-        assertEquals('mad',o.check(s));
-
+        assertEquals("mad", o.check("mad"));
     }
 }
